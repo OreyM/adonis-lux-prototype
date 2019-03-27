b3e232065e881da16c35a007d61d28f65a727289
OreyM <oreymgt@gmail.com>
2019-03-27 17:15:29 +0200

    main page create. header struct. top nav struct

---
+++ js/common.js
@@ -0,0 +1,11 @@
+//TO DELETE
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('jQuery connect!');
+});
+//END TO DELETE
+
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $(".dropdown-trigger").dropdown();
+});
